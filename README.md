3d161d99f6311a1e4dc452c75b5ef9f40647218d
Ryan vanniekerk <iamnayr@gmail.com>
2020-04-02 16:32:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Half-Life Alyx SDK Description
+The purpose of this repo is to automate the process of enabling the SteamVR environments tools to be used to create and compile maps for use with Half Life Alyx.  Big credit for this script goes to Gvarados [twitch](https://www.twitch.tv/gvarados) as I learned how to do this by watching his live stream while he set this up and painstakingly recreated it on my own.
+
+# How-To
+This repo includes a batch (.bat) script that you can run on your machine to faciliate the creation of a Alyx SDK sourcemod.  This can be run as a standalone application that will enable you to use all of the Half-Life Alyx assets to start creating custom Half Life Alyx maps before the official toolkit arrives!  This works because the existing SteamVR environments editor is ALSO based on source 2 and fairly compatible with HL Alyx assets.
+
+## Run the .bat script
+The script tries to make some assumptions about where your steam/alyx/steamvr installs are located, but should prompt you if any/all of those don't exist and you will need to manually enter the path.  The script will copy over files that it needs (~2GB) to launch the editor. 
+
+## Follow additional instructions at the end of .bat script
+Please note that the SDK that gets created DOES NOT include the HL Alyx assets,  these come from `pak_01.dir` in your EXISTING ACTUAL `Half-Life Alyx/game/hlvr` directory.  This script does nothing to automate the extraction of those assets into the newly created `Half-Life Alyx SDK` directory.   This is a manual process and you will have to use a tool like GCFScape or VRF to extract the folders from the VPK file into the `Half-Life Alyx SDK/game/hlvr/` directory created by this script. 
+
+You MUST ALSO download the `.FGD` files required by Half-Life Alyx to load entities into Hammer or none of the assets will work correctly on your map.  You can download those from [Gvarados1 FGD repo](https://github.com/gvarados1/Half-Life-Alyx-FGD)
+
+## Compiling maps
+Maps *should* compile from the hammer editor, note that you will have to COPY them over to your REAL Half-Life Alyx maps folder in order to actually try them out.  This is possible with +sv_cheats and the `map` command in the console of Half Life Alyx.  
+
+# Why?
+Valve initially promised us SDK tools that would launch side-by-side with Half-Life Alyx, but it turns out that wasn't the case, although they have now informed us they are hard at work at releasing the SDK and/or editor.
+
+# DISCLAIMER
+I'm not responsible for anything that happens to your machine if you run this script, please don't sue me.  In addition I'm a complete novice to the Hammer editor, so I have no notion of what is/isn't functioning properly when it comes to that,  but basic world building and assett dropping seems to be working perfectly fine. 
---
+++ hl-alyx-sdk-setup.bat
@@ -0,0 +1,105 @@
+@ECHO OFF
+ECHO NOTE
+ECHO Please follow instructions at the end of script to extract / download additional needed files!
+ECHO :::::::::::::::::::::::::::::::::::::::
+SET steam_default_dir=C:/Program Files (x86)/Steam/steamapps
+:: Steam directory check
+SET /P steam_defaults=Use default steam directory %steam_default_dir% (Y\N)?
+IF NOT "%steam_defaults%"=="Y" GOTO SetupSteamDir
+GOTO:file_copy_prompt
+
+:SetupSteamDir
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Using custom steam directory
+ECHO :::::::::::::::::::::::::::::::::::::::
+SET /P steam_default_dir=Enter location of steamapps directory:
+
+:file_copy_prompt
+IF exist "%steam_default_dir%" ( echo Found steam directory, continuing ) ELSE ( GOTO DontRun )
+SET /P continue_file_copy=Continue SDK setup, requires ~2GB for base install (Y/N)
+IF NOT "%continue_file_copy%"=="Y" GOTO DontRun
+
+:find_alyx
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Looking for Half life alyx directory
+ECHO :::::::::::::::::::::::::::::::::::::::
+IF exist "%steam_default_dir%/common/Half-Life Alyx/" ( SET "alyx_dir=%steam_default_dir%/common/Half-Life Alyx/" ) ELSE ( SET /P alyx_dir=Couldn't find alyx, please specify directory to Half-Life Alyx: )
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Alyx dir is set to %alyx_dir%
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+:find_steamvr
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Looking for steamvr tools
+ECHO :::::::::::::::::::::::::::::::::::::::
+IF exist "%steam_default_dir%/common/SteamVR/" ( SET "steamvr_dir=%steam_default_dir%/common/SteamVR" ) ELSE ( SET /P steamvr_dir=Couldn't find SteamVR tools, please specify directory to SteamVR: )
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO SteamVR dir is set to %steamvr_dir%
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+:find_sourcemods
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Looking for sourcemods folder
+ECHO :::::::::::::::::::::::::::::::::::::::
+IF exist "%steam_default_dir%/sourcemods/" ( SET "sourcemods_dir=%steam_default_dir%/sourcemods" ) ELSE ( SET /P sourcemods_dir=Couldn't find sourcemods dir, please specify directory to sourcemods: )
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Sourcemods dir is set to %sourcemods_dir%
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+:file_copy
+:: Prepare Alyx SDK directory
+SET "alyx_sdk_dir=%sourcemods_dir%/Half-Life Alyx SDK"
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Prepping Half Life Alyx SDK directories
+ECHO :::::::::::::::::::::::::::::::::::::::
+IF NOT exist "%alyx_sdk_dir%" ( ECHO it doesn't exist, creating & mkdir "%alyx_sdk_dir%" "%alyx_sdk_dir%/game" ) ELSE ( ECHO it already exists )
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Copying SteamVR files....
+ECHO :::::::::::::::::::::::::::::::::::::::
+robocopy "%steamvr_dir%/tools/steamvr_environments/game/bin" "%alyx_sdk_dir%/game/bin" /s /e /nfl /ndl /njh 
+robocopy "%steamvr_dir%/tools/steamvr_environments/game/core" "%alyx_sdk_dir%/game/core" /s /e /nfl /ndl /njh /XF pak* /XF shaders*
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Done copying Steam VR files.
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Copying Half Life Alyx files...
+ECHO :::::::::::::::::::::::::::::::::::::::
+robocopy "%alyx_dir%/game/hlvr" "%alyx_sdk_dir%/game/hlvr" /s /e /nfl /ndl /njh /XF pak* /XD maps
+robocopy "%alyx_dir%/game/bin/win64" "%alyx_sdk_dir%/game/bin/win64" hlvr.exe
+robocopy "%alyx_dir%/game/core" "%alyx_sdk_dir%/game/core" pak* /nfl ndl /njh 
+robocopy "%alyx_dir%/game/core" "%alyx_sdk_dir%/game/core" shaders* /nfl ndl /njh 
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Done copying Half Life Alyx files.
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Replacing Half-Life Alyx dll's
+ECHO :::::::::::::::::::::::::::::::::::::::
+robocopy "%steamvr_dir%/tools/steamvr_environments/game/steamtours/bin/win64" "%alyx_sdk_dir%/game/hlvr/bin/win64" /s /e /nfl /ndl /njh
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Done replacing Half-Life Alyx dll's
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+GOTO Success
+
+:Success
+ECHO Copied files successfully 
+ECHO ::::::::::::::::NOTE:::::::::::::::::::
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO You MUST extract pak_01.dir file from Half Life Alyx game directory into %alyx_sdk_dir%/game/hlvr
+ECHO You MUST download .FGD files from https://github.com/gvarados1/Half-Life-Alyx-FGD and extract them into %alyx_sdk_dir%/game/hlvr
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+ECHO DONE...Happy mapping.
+GOTO:EOF
+
+:DontRun
+ECHO :::::::::::::::::::::::::::::::::::::::
+ECHO Stopping HL Alyx editor setup (cancelled)
+ECHO :::::::::::::::::::::::::::::::::::::::
+
+
+PAUSE
