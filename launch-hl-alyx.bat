3dcda7642a6a049c7f23b0903a6151866ff72a5e
Juest Zungo <Juesto@users.noreply.github.com>
2020-04-09 02:53:54 -0300

    Update launch-hl-alyx.bat

--- launch-hl-alyx.bat
+++ launch-hl-alyx.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Steam\steamapps\sourcemods\Half-Life Alyx SDK\game\bin\win64\hlvr.exe" -sw -tools -toolsvr -novr -vconsole -noasserts -nopassiveasserts -dev -devcomp -nop4
+game\bin\win64\hlvr.exe -sw -tools -toolsvr -novr -vconsole -noasserts -nopassiveasserts -dev -devcomp -nop4
